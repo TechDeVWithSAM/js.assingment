ab90e69f03c5e77644edf375b39c41f6532ec8d6
Salman <Salmanrajputsr71@gmail.com>
2022-03-27 13:52:56 -0700

    Work Accomplish

---
+++ chapter (1-5)/app.js
@@ -0,0 +1,249 @@
+/////////////////////////// Chapter 1 ////////////////////////////////
+
+
+// Q1
+// alert("Welcome to this Page");
+
+// Q2
+// alert("Error! Please enter a valid password");
+
+// Q3
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// Q4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// Q5
+// console.log ("Hello... I can run JS through my browser’s console")
+
+// Q7
+// All
+
+
+
+
+///////////////////////////// Chapter 2 //////////////////////////////
+
+
+// Q1
+// var username="Salman";
+// alert(username);
+
+// Q2
+// var username="Salman";
+// alert(username);
+
+// Q3
+// var message;
+// message="hello world";
+// alert (message);
+
+// Q4
+// var studentname="Salman";
+// alert(studentname);
+
+// var StudentAge="19 Years Old";
+// alert(StudentAge)
+
+// var StudentCourse="Web and App Course";
+// alert(StudentCourse);
+
+// Q5
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// Q6
+// var email = "salmanrajputsr71@gmail.com";
+// alert("My Email Address is:" +email);
+
+// Q7
+// var book="A smaeter\n way to learn Javascript";
+// alert("I am trying to learn from the book" +book);
+
+// Q8
+// document.write("Yah ! I can write HTML content through JavaScript");
+
+// Q9
+// var a ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(a);
+
+
+
+
+////////////////////////////// Chapter 3 /////////////////////////////
+
+  
+// Q1
+// var age = "I am 32 Years old";
+// alert(age);
+
+
+// Q2
+// var numberOfvisits ="You have visited this site 14 times";
+// alert(numberOfvisits);
+
+// Q3
+// var birthYear =1988;
+// document.write("My Birth Year Is" +" " +birthYear+ "<br>" + "Data Type OF My Declared Variable Is Number");
+// document.write("<br><br>");
+
+// Q4
+// document.write("<b>" + "John Doe" + "</b>" + " " + "ordered" + " " + "<b>" + "5" + "  " + "T-shart" +"</b>" + "(s)" + " " + "on XYZ Clothing store");
+
+
+
+
+
+//////////////////////////////// Chapter 4 ///////////////////////////
+
+
+// Q1
+// var variable1 = "Hello World!" + " ",
+//     variable2 = "Salman Testing..." + " ",
+//     variable3 = 20;
+// alert(variable1 + variable2 + variable3);
+
+
+// Q2
+
+//Illegal Variable Names
+//var _9w;
+//var %jaz;
+//-conting = 24;
+//var ,lastname = "man";
+//Gethours;
+
+//Legel Variable Names
+//var manSalman;
+//var $_;
+//var booHoo;
+//var firstLegalChar = "1";
+//var topScore = 100;
+//var total = cost + profit;
+
+
+// Q3
+// document.write("<p style='font-size:30px;'>" + "Rules for naming JS variables" + "</p>" + "<br/>");
+// document.write("<p>" + "Variable names can only contain" + " " + "," + " " + "numbers" + "," + " " + "$ and" + " " + " _" + " " + "." + " " + "For example :"+  " " + "$my" + " " + "_ 1st Variable" + "<br/>" + "Variables must begin with a letter, $ or _ . For Example: $name, _ name or name" + "</br>" + "Variable names are case" + " " + "sensitive" + "</br>" + "Variable names should not be JS" + " " + "Keywords" + "</p>");
+
+
+
+
+/////////////////////////////// Chapter 5 ////////////////////////////
+
+// Q1
+// var a = 3;
+// var b = 5;
+// var c = a + b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+
+// Q2
+//for subtraction
+// var a = 3;
+// var b = 5;
+// var c = a - b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+//for multiplication
+// var a = 3;
+// var b = 5;
+// var c = a * b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+//for  division
+// var a = 3;
+// var b = 5;
+// var c = a / b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+//for  modulus
+// var a = 3;
+// var b = 5;
+// var c = a % b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+
+
+// Q3
+// var a;
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Value after variable declaration is" + "  " );
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Initial Value:" + "  "  +  " ");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Value after increment is:" + "  " );
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Value after addition is:" + "  " );
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Value after decrement is:" + "  ");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "The remainder is:" + "  ");
+
+
+// Q4
+// var ticketPrice = 600;
+// var b = 5;
+// var c = ticketPrice * b;
+// document.write("<h3>" + "Total cost to buy" + "  " + b + " " + "tickets to a movie is" + " " + c + "PKR"+ "</h3>");
+
+
+
+// Q5
+// var x = 4, i;
+// for(i=1;i<=10;i++)
+// document.write("<h3>"  + "4x" + i + "=" + i*x + "</h3>");
+
+
+// Q6
+// var celSius=30, fahreNheit=86, conversion1, conversion2;
+// conversion1=(fahreNheit-32)*5/9;
+// conversion2=(celSius*9/5)+32
+// document.write("Fahrenheit Is:"+ " " + conversion1 + "<br/>" + "Celsius Is:" + " " + conversion2);
+
+
+// Q7
+// var  priceOfitem1 = 650,  priceOfitem2 = 100, quantity1 = 3, quantity2 = 7, shippingCharges = 100, 
+// totalCost =priceOfitem1*quantity1+priceOfitem2+shippingCharges*quantity2;
+// document.write("<h1>"+ "Shopiing Cart " + "</h1>" + "<br/>");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + " Price of item 1 is" + " " + priceOfitem1 + "<br/>" + "Quantity of item 1 is" + " " + quantity1 + "<br/>" + "Price of item 2 is" + " " + priceOfitem2 + "<br/>" + "Quantity of item 2 is" + " " + quantity2 + "<br/>" + " Sipping Charges" + " " + shippingCharges + "<br/>" +  "<br/>" + "Total cost of your ordered is" + " " + totalCost + "</p>");
+
+
+
+// Q8
+// document.write("<h1>"+ "Marks Sheets " + "</h1>" + "<br/>");
+// var total = "980", ob = "804", per;
+// per=ob*100/total;
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Total Marks:" + " " + total + "<br/>" + "Marks Obtained:" + " " + ob + "<br/>" + "Percentage" + " " + per + "</p>");
+
+
+
+// Q9
+// var usDollars = 104.80*10, saudiRiyals = 25*28, pakistaniRupee;
+//  pakistaniRupee= usDollars+saudiRiyals;
+// document.write("<h1>"+ "Currency In PKR" + "</h1>" + "<br/>");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Total Currency in PKR:" + " " + pakistaniRupee + "</p>");
+
+
+// Q10
+// a=5, b=2, c=a*b, d=c/a;
+// document.write("<p style='font-size: 20px; font-weight: 600;'>" + "a. Add" + " " + a + "<br/>" + "b. Multiply by" + " " + c + "<br/>" + "c. Divide the result by" + " " + d + "</p>");
+
+
+// Q11
+// document.write("<h1 style='font-size: 38px; font-weight: 900;'>" + "Age Calculator" + "</h1>" + "<br/>");
+// var currentYear = 2020, birthYear = 1999, age=currentYear-birthYear;
+// document.write("<p style='font-size: 22px; font-weight: 500;'>" + "Current Year: " + " " + currentYear + "<br/>" + "Birth Year:" + " " + birthYear + "<br/>" + "Your Age Is:"+ " " + age);
+
+
+
+// Q12
+// var area, r, pi = 3.142, coc;
+// r=parseInt(prompt("Enter The Radius"));
+// area=pi*r*r;
+// coc=2*pi*r;
+// document.write("<h1 style='font-size: 38px; font-weight: 900;'>" + "The Geometrizer" + "</h1>" + "<br/>");
+// document.write("<p style='font-size: 22px; font-weight: 500;'>" + "Radius of a circle: " + " " + r + "<br/>" + "The circumference is:" + " " + coc +  "<br/>" + "The area is:" + " " + area + "</p>");
+
+
+
+// Q13
+// var favoriteSnack="Potato Chips", currentAge=21,  maximumAge=70,  amountPerday=2, totalForrest;
+// totalForrest = currentAge+maximumAge*amountPerday;
+// document.write("<h1 style='font-size: 38px; font-weight: 900;'>" + "The Lifetime Supply Calculator" + "</h1>" + "<br/>");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Favourite Snack:"  + " " + favoriteSnack + "<br/>" + "Current Age:" + " " + currentAge + "<br/>" + "Extimated Maximum Age:"+ " " + maximumAge + "<br/>" + "Amount Of Snacks Per Day" + " " + amountPerday + "<br/>" + "You will need" + " " + totalForrest + " " + "to last you until the ripe old age of" + "  " + maximumAge + "</p>" );
+
